81bd1dd28c8e144a98f37dc78f9dbfdd7d8103ee
lethbrandao <leticiacassiano1947@hotmail.com>
2021-09-27 21:11:03 -0300

    upload

---
+++ Lista 06.py
@@ -0,0 +1,21 @@
+# 1 - imports - bibliotecas
+import pytest
+
+
+# 2 - class - classe
+
+# 3 - definitions - definições = métodos e funções
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+def calcular_fosforos(numerocaixas):
+    assert numerocaixas * 40
+        resultado = numerocaixas * 40
+    else:
+        resultado = 'Não multiplicarás por zero'
+    return resultado
+
+
+        (f'Informe o número de caixas')
